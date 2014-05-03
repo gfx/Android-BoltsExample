7812c603973b0fb7e452be252ebd3bebc4f52680
Fuji, Goro (gfx) <gfuji@cpan.org>
2014-05-03 20:17:34 +0900

    initial commit

---
+++ app/src/main/java/com/github/gfx/boltsexample/app/MainActivity.java
@@ -0,0 +1,14 @@
+package com.github.gfx.boltsexample.app;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
